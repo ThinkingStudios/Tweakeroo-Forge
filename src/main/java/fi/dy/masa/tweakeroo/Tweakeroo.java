25a693da7832eceb40c18886cf32c5810bc0254b
TexTrue <65154269+TexBlock@users.noreply.github.com>
2023-06-14 12:28:42 +0800

    fix

--- src/main/java/fi/dy/masa/tweakeroo/Tweakeroo.java
+++ src/main/java/fi/dy/masa/tweakeroo/Tweakeroo.java
@@ -1,6 +1,11 @@
 package fi.dy.masa.tweakeroo;
 
+import fi.dy.masa.malilib.compat.forge.ForgePlatformCompat;
+import fi.dy.masa.tweakeroo.gui.GuiConfigs;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import fi.dy.masa.malilib.event.InitializationHandler;
@@ -14,6 +19,19 @@ public class Tweakeroo
     public static int renderCountXPOrbs;
 
     public Tweakeroo(){
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::onInitializeClient);
+    }
+
+    public void onInitializeClient(FMLClientSetupEvent event) {
+        ForgePlatformCompat.getInstance().getModClientExtensionPoint();
         InitializationHandler.getInstance().registerInitializationHandler(new InitHandler());
+
+        ForgePlatformCompat.getInstance().getMod(Reference.MOD_ID).registerModConfigScreen((screen) -> {
+            GuiConfigs gui = new GuiConfigs();
+            gui.setParent(screen);
+            return gui;
+        });
     }
 }
