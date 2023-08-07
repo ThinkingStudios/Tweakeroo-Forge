40c706c16c78e0942b313806a64941374dd186d3
Kasualix <670067575@qq.com>
2023-08-07 18:11:23 +0800

    fix other mixins' issues

--- src/main/java/fi/dy/masa/tweakeroo/mixin/MixinUpdateStructureBlockC2SPacket.java
+++ src/main/java/fi/dy/masa/tweakeroo/mixin/MixinUpdateStructureBlockC2SPacket.java
@@ -17,13 +17,13 @@ public abstract class MixinUpdateStructureBlockC2SPacket
                                       target = "Lnet/minecraft/network/packet/c2s/play/UpdateStructureBlockC2SPacket;structureName:Ljava/lang/String;"),
                            to   = @At(value = "FIELD", opcode = Opcodes.PUTFIELD,
                                       target = "Lnet/minecraft/network/packet/c2s/play/UpdateStructureBlockC2SPacket;mirror:Lnet/minecraft/util/BlockMirror;")),
-            constant = { @Constant(intValue = -32), @Constant(intValue = 32) }, require = 0)
-    private int overrideStructureBlockSizeLimit(int original)
+            constant = { @Constant(intValue = -48), @Constant(intValue = 48) }, require = 0)
+    private int tweakeroo_overrideStructureBlockSizeLimit(int original)
     {
         if (FeatureToggle.TWEAK_STRUCTURE_BLOCK_LIMIT.getBooleanValue())
         {
             int overridden = Configs.Generic.STRUCTURE_BLOCK_MAX_SIZE.getIntegerValue();
-            return original == -32 ? -overridden : overridden;
+            return original == -48 ? -overridden : overridden;
         }
 
         return original;
