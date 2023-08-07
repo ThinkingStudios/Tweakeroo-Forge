ba3c148d3045c6eae46190958f87895780cba8a7
Kasualix <670067575@qq.com>
2023-08-07 17:57:17 +0800

    fix crash on startup, close #2 , close #1

--- src/main/java/fi/dy/masa/tweakeroo/mixin/MixinWorld.java
+++ src/main/java/fi/dy/masa/tweakeroo/mixin/MixinWorld.java
@@ -1,8 +1,10 @@
 package fi.dy.masa.tweakeroo.mixin;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.function.Consumer;
+import fi.dy.masa.tweakeroo.config.Configs;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -10,11 +12,10 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.world.World;
-import fi.dy.masa.tweakeroo.config.Configs;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.function.Consumer;
 
 @Mixin(World.class)
 public abstract class MixinWorld
@@ -53,15 +54,14 @@ public abstract class MixinWorld
     }
 
     @Inject(method = "tickBlockEntities",
-            at = @At(value = "FIELD", target = "Lnet/minecraft/world/World;unloadedBlockEntities:Ljava/util/List;", ordinal = 0))
+            at = @At(value = "FIELD", target = "Lnet/minecraft/world/World;unloadedBlockEntities:Ljava/util/Set;", ordinal = 0))
     private void optimizedTileEntityRemoval(CallbackInfo ci)
     {
         if (Configs.Fixes.TILE_UNLOAD_OPTIMIZATION.getBooleanValue())
         {
             if (!this.unloadedBlockEntities.isEmpty())
             {
-                HashSet<BlockEntity> remove = new HashSet<>();
-                remove.addAll(this.unloadedBlockEntities);
+                HashSet<BlockEntity> remove = new HashSet<>(this.unloadedBlockEntities);
 
                 this.tickingBlockEntities.removeAll(remove);
                 this.blockEntities.removeAll(remove);
