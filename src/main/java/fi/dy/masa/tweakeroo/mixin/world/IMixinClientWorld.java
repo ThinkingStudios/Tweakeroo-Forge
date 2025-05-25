c8731a8e8b5c9ad779206fdd74bd1aea444faeb6
TexTrue <65154269+TexBlock@users.noreply.github.com>
2025-05-25 14:57:30 +0800

    sync 1.21.4-0.23.3, update mafglib, update NeoForge version to `21.4.136`

---
+++ src/main/java/fi/dy/masa/tweakeroo/mixin/world/IMixinClientWorld.java
@@ -0,0 +1,15 @@
+package fi.dy.masa.tweakeroo.mixin.world;
+
+import java.util.Map;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.component.type.MapIdComponent;
+import net.minecraft.item.map.MapState;
+
+@Mixin(ClientWorld.class)
+public interface IMixinClientWorld
+{
+    @Invoker("getMapStates")
+    Map<MapIdComponent, MapState> tweakeroo_getMapStates();
+}
